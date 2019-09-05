cd23271ec27125dd2fa60a2fc42118784848c30b
RedPlus <redplus@redplus.net>
2019-09-05 16:39:18 +0900

    코드 정리

---
+++ KotlinNote/15_Else/ElseDemo.kt
@@ -0,0 +1,13 @@
+﻿fun main(args: Array<String>) {
+    val score = 80
+    
+    val result = if (score >= 80) {
+        println("합격");
+        "합격";
+    } else {
+        println("불합격");
+        "불합격";
+    }
+    
+    println("$result");    
+}
---
+++ KotlinNote/16_Switch/WhenDemo.kt
@@ -0,0 +1,13 @@
+﻿fun main(args: Array<String>) {
+    val score = 80
+
+	when(score) {
+		100 -> println("좋아")
+		80 -> println("나쁘지않아")
+		else -> {
+			println("다른 점수")
+		}
+	}
+    
+    println("$result");    
+}
